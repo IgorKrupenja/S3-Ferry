2a2664c82aaa348e6a30bdaec0056f9027c9ae76
Igor Krupenja <igor.krupenja@gmail.com>
2026-01-30 15:44:14 +0200

    feat(1): fix optional

--- src/fs/config/fs.config.schema.ts
+++ src/fs/config/fs.config.schema.ts
@@ -1,7 +1,7 @@
 import * as joi from 'joi';
 
 const schema = {
-  FS_DATA_DIRECTORY_PATH: joi.string(),
+  FS_DATA_DIRECTORY_PATH: joi.string().optional(),
 };
 
 export const fsConfigSchema = joi.object<typeof schema>(schema);
--- src/s3/config/s3.config.schema.ts
+++ src/s3/config/s3.config.schema.ts
@@ -5,8 +5,8 @@ const schema = {
   S3_ENDPOINT_URL: joi.string().uri().allow(''),
   S3_ACCESS_KEY_ID: joi.string(),
   S3_SECRET_ACCESS_KEY: joi.string(),
-  S3_DATA_BUCKET_NAME: joi.string(),
-  S3_DATA_BUCKET_PATH: joi.string().allow(''),
+  S3_DATA_BUCKET_NAME: joi.string().optional(),
+  S3_DATA_BUCKET_PATH: joi.string().optional().allow(''),
 };
 
 export const s3ConfigSchema = joi.object<typeof schema>(schema);
